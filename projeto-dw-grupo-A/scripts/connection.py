e23c344874a2d29d2c6af7a257033c47b930625e
Matheus Mendonça dos Santos <87781736+RockMatalacth@users.noreply.github.com>
2025-11-20 11:03:25 -0300

    conection
    atualizacao da conexao com banco

--- projeto-dw-grupo-A/scripts/connection.py
+++ projeto-dw-grupo-A/scripts/connection.py
@@ -9,7 +9,7 @@ DB_USER = "avnadmin"
 DB_NAME = "defaultdb"
 DB_HOST = "pg-602cf04-teusmath89-cc57.h.aivencloud.com"
 DB_PORT = 23430
-DB_PASSWORD = "AVNS_Q2o5_cku1dndcK4X0nt" 
+DB_PASSWORD = "" 
 
 DATABASE_URL = (
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
